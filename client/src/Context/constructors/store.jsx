1a81a3eb0fbcb4ab0a18a344267738bc94fb0ce9
douglas86 <douglasmaxton@gmail.com>
2022-12-12 16:09:05 +0000

    Created my state store

---
+++ client/src/Context/constructors/store.jsx
@@ -0,0 +1,16 @@
+import { createContext, useMemo, useReducer } from "react";
+import { rootReducers } from "./rootReducers";
+
+const initialState = {};
+
+export const Context = createContext(initialState);
+
+export const Provider = ({ children }) => {
+  const { state, dispatch } = useReducer(rootReducers, initialState);
+
+  const contextValue = useMemo(() => {
+    return { state, dispatch };
+  }, [state, dispatch]);
+
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+};
--- client/src/constructors/App.jsx
+++ client/src/constructors/App.jsx
@@ -1,39 +1,12 @@
-// 1. Testing Front and Backend with server proxy and useState & useEffect (It's working!)
-
-// import React, { useEffect, useState } from "react";
-
-// const App = () => {
-//   const [backendData, setBackendData] = useState([{}]);
-
-//   useEffect(() => {
-//     fetch("/api")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setBackendData(data);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       {typeof backendData.users === "undefined" ? (
-//         <p>Loading...</p>
-//       ) : (
-//         backendData.users.map((users, i) => <p key={i}>{users}</p>)
-//       )}
-//     </div>
-//   );
-// };
-
-//
-
-// 2. Testing Front and Backend with PostgreSQL
-
 import React from "react";
+import { Provider } from "../Context/constructors/store";
 
 const App = () => {
   return (
-    <div>Hello Team</div>
-  )   
+    <Provider>
+      <div>Hello Team</div>
+    </Provider>
+  );
 };
 
 export default App;
