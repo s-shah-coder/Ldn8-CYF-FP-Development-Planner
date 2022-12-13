e8ddb9b51e60d918c166ecd25fbc50ae86f47d91
hachi-ops <g.helen.rog@gmail.com>
2022-12-13 16:36:23 +0000

    remove code

--- server/database.js
+++ server/database.js
@@ -5,23 +5,7 @@ const pool = new Pool({
   password: "root",
   host: "localhost",
   port: 5432,
-  database: "dev_plan_login"
+  database: "dev_plan_login",
 });
-
-// const createTblQuery = `CREATE TABLE users (
-//     user_id serial PRIMARY KEY,
-//     username VARCHAR ( 50 ) UNIQUE NOT NULL,
-//     password VARCHAR ( 50 ) UNIQUE NOT NULL
-// );`
-
-// pool
-//   .query(createTblQuery)
-//   .then((Response) => {
-//     console.log("Table Created");
-//     console.log(response);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
 
 module.exports = pool;
