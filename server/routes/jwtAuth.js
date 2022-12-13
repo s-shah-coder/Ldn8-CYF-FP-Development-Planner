75402552dfb75d0f428ef2f1de0e6592f23357af
hachi-ops <g.helen.rog@gmail.com>
2022-12-13 13:56:16 +0000

    brypt password and add new user

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const pool = require("../database");
+const bcrypt = require("bcrypt");
 
 //register new user
 router.post("/register", async (req, res) => {
@@ -14,12 +15,24 @@ router.post("/register", async (req, res) => {
     if (user.rows.length !== 0) {
       return res.status(401).send("User already exists");
     }
+
     res.json(user.rows);
 
     // 3. bcrypt the user password
 
+    const saltRound = 10;
+    const salt = await bcrypt.genSalt(saltRound);
+
+    const bcryptPassword = await bcrypt.hash(password, salt);
+
     // 4. insert new user inside the database
 
+    const newUser = await pool.query(
+      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [username, email, bcryptPassword]
+    );
+
+    res.json(newUser.rows[0]);
     // 5. generate jwt token
   } catch (err) {
     console.error(err.message);
